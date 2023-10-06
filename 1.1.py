b96b1b2c6ea1b7ce4a54e7788100849506bc70e9
PoChieh-workplace <justin07249495@gmail.com>
2023-10-07 00:30:51 +0800

    1.1

--- 1.1.py
+++ 1.1.py
@@ -1,17 +1,2 @@
 # 1.1
 print(int(input())%10)
-
-# 1.2
-a = input()
-ind = a.find(".")
-print(a.replace(a[:ind],"0") if ind!=-1 else 0)
-
-
-
-# 1.4
-a = int(input())
-tmp = 0
-while(a!=0):
-    tmp+=a%10
-    a/=10
-print(tmp)
